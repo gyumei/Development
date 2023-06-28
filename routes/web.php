a381a10a393a8d2e00ba6a7a515f88d9c0646312
gyumei <hannibal221b219@gmail.com>
2023-06-29 00:27:16 +0900

    いい加減にしてくれ

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::get('/like', function() {
 
 
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
